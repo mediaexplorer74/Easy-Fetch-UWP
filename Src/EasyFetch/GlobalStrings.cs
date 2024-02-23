a6e1aa227a4283df5ca39ef466ab47270fc9c7d9
Andrey Zakharov <mediaexplorer74@hotmail.com>
2024-02-23 16:16:01 +0300

    EasyFetch v1.2-alpha

---
+++ Src/EasyFetch/GlobalStrings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone_Helper
+{
+    class GlobalStrings
+    {
+        public static string Downloaders_GeneralDownloaderFileName { get; set; }
+        public static bool FileNameAccepted { get; set; }
+        public static string Logger { get; set; }
+    }
+}
